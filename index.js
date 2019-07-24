0d0a4d93e555fcb7dd891d4e688c7ed2d94539fb
PascalUlor <pascalulor@yahoo.com>
2019-07-24 15:19:39 +0100

    initial commit for jwt project set up server install dependencies and dev dependencies

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
